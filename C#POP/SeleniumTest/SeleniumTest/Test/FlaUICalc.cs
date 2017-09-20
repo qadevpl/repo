8b47c36c6a10d95c760ff3a0241526e7f77852a8
qadevpl <michal.ozibko@gmail.com>
2017-09-20 14:53:54 +0200

    Add new tests explore tools: FlaUICalc

---
+++ C#POP/SeleniumTest/SeleniumTest/Test/FlaUICalc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using FlaUI.Core;
+using FlaUI.UIA3;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.AutomationElements.Infrastructure;
+
+namespace SeleniumTest.Test
+{
+    [TestFixture]
+    public class FlaUICalc
+    {
+        public object Find { get; private set; }
+
+        [Test]
+        public void FlaUICalcTest()
+        {
+            var app = Application.LaunchStoreApp("Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            app.WaitWhileMainHandleIsMissing();
+
+            using (var automation = new UIA3Automation())
+            {
+                var window = app.GetMainWindow(automation);
+
+                Button bMenu = window.FindFirstDescendant(cf => cf.ByName("Menu")).AsButton();
+                System.Threading.Thread.Sleep(1000);
+                bMenu.Click();
+
+                Tree listMenu = window.FindFirstDescendant(cf => cf.ByName("Lista rozwijana trybów")).AsTree();
+                listMenu.FindFirstDescendant(cf => cf.ByName("Naukowy Kalkulator")).AsButton().Click();
+
+                window.FindFirstDescendant(cf => cf.ByName("Jeden")).AsButton().Click();
+                window.FindFirstDescendant(cf => cf.ByName("Plus")).AsButton().Click();
+                window.FindFirstDescendant(cf => cf.ByName("Dwa")).AsButton().Click();
+                window.FindFirstDescendant(cf => cf.ByName("Równa się")).AsButton().Click();
+
+                //check result
+                var resultElement = window.FindFirstDescendant(cf => cf.ByAutomationId("CalculatorResults"));
+                var resultValue = resultElement.Properties.Name;
+
+                Assert.AreEqual("Wyświetlana wartość to 3", resultValue);
+            }
+        }
+    }
+}
