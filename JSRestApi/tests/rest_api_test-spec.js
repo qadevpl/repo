eca9f8b798cf75c4aee341c54c02e87fe4ad6ae1
Michał <michalmin@gmail.com>
2018-04-10 16:32:51 +0200

    Add debug mode for VSCode and package.json

--- JSRestApi/tests/rest_api_test-spec.js
+++ JSRestApi/tests/rest_api_test-spec.js
@@ -1,10 +1,11 @@
-beforeAll(function(){
-});
+const request = require('request');
+const jsonPlaceHolderURL = "https://jsonplaceholder.typicode.com/posts/?userId=5";
 
-beforeEach(function(){
-});
-
-describe('test RestApi', function() {
-    it('test for services', function() {
+describe('test RestApi', () => {
+    it('test for services', (done) => {
+            request.get(jsonPlaceHolderURL, (err, res, body) => {  
+            expect(res.statusCode).toBe(200);
+            done();
+           });
     });
 });
